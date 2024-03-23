575e9a7c21e4d47c1d3f41fd01a3635f9d98bc47
duclinh21122000 <duongduclinh2207@gmail.com>
2024-03-23 08:52:38 +0700

    feat: ui footer

--- src/utils/data/index.tsx
+++ src/utils/data/index.tsx
@@ -14,3 +14,50 @@ export const SLIDER_BANNER = [
     image: IMAGE_SLIDER_3.src,
   },
 ]
+
+export const TAG_FOOTER = [
+  {
+    id: 1,
+    content: 'Truyện Tranh',
+  },
+  {
+    id: 2,
+    content: 'Truyện Tranh',
+  },
+  {
+    id: 3,
+    content: 'Truyện Tranh',
+  },
+  {
+    id: 4,
+    content: 'Truyện Tranh',
+  },
+  {
+    id: 5,
+    content: 'Truyện Tranh',
+  },
+  {
+    id: 6,
+    content: 'Truyện Tranh',
+  },
+  {
+    id: 7,
+    content: 'Truyện Tranh',
+  },
+  {
+    id: 8,
+    content: 'Truyện Tranh',
+  },
+  {
+    id: 9,
+    content: 'Truyện Tranh',
+  },
+  {
+    id: 10,
+    content: 'Truyện Tranh',
+  },
+  {
+    id: 11,
+    content: 'Truyện Tranh Online',
+  },
+]
